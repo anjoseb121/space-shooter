af34dbfbd0e13e560c2a00d81ef80d1e6a7fb231
Antonio Barandica <anjose.121@gmail.com>
2017-07-22 13:14:49 -0500

    Add destroy asteroid with laser

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        // Destroy the object who touch de asteroid
+        Destroy(other.gameObject);
+        // Destroy the asteroid
+        Destroy(gameObject);
+    }
+}
