c77340c990267d1d7396e3f852fa7b789679658a
Antonio Barandica <anjose.121@gmail.com>
2017-07-22 23:33:16 -0500

    Add end game logic. Add restart game event

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,15 +11,35 @@ public class GameController : MonoBehaviour {
     public float waveWait;
 
     public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
     private int score;
     
 	// Use this for initialization
 	void Start ()
     {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
         score = 0;
         UpdateScore();
         StartCoroutine (SpawnWaves());
 	}
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
 
     IEnumerator SpawnWaves()
     {
@@ -34,6 +54,13 @@ public class GameController : MonoBehaviour {
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
         }
     }
 
@@ -48,4 +75,9 @@ public class GameController : MonoBehaviour {
         scoreText.text = "Score: " + score;
     }
 
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
 }
