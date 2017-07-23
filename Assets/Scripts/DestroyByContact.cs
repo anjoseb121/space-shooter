76e56398d0770d8b8ead21557e702cf704cf2545
Antonio Barandica <anjose.121@gmail.com>
2017-07-22 19:23:08 -0500

    Add asteroid as prefab. Add VFX to asteroid

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject playerExplosion;
+    public GameObject explosion;
+
     private void OnTriggerEnter(Collider other)
     {
+        // Don't destroy the boundary
         if (other.tag == "Boundary")
         {
             return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player") { 
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
         // Destroy the object who touch de asteroid
         Destroy(other.gameObject);
